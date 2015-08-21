9bf2108988f393c0d467b534f2d57200cd8753b0
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-08-21 09:55:29 -0500

    Switching to a better benchmark style

--- benchmarks/earhart_vs_world.rb
+++ benchmarks/earhart_vs_world.rb
@@ -15,33 +15,33 @@ puts ""
 
 Benchmark::IPS.options[:format] = :raw
 
+EarhartTestRouter = Earhart::Router.new do |router|
+  router.collection(verb: :post, resource: :accounts, receiver: Object)
+  router.collection(verb: :get, resource: :accounts, receiver: Object)
+  router.member(verb: :get, resource: :accounts, receiver: Object)
+  router.member(verb: :put, resource: :accounts, receiver: Object)
+  router.member(verb: :delete, resource: :accounts, receiver: Object)
+end
+
+LotusTestRouter = Lotus::Router.new do
+  resource "accounts"
+end
+
+LotusRouterRack = Rack::MockRequest.new(LotusTestRouter)
+
 Benchmark.ips do |analysis|
   analysis.time = 5
   analysis.warmup = 2
 
   analysis.report("earhart lookup") do
-    dispatch = Earhart::Router.new do |router|
-      router.map(:accounts) do |endpoint|
-        collection(endpoint, :get, Object)
-        collection(endpoint, :post, Object)
-        member(endpoint, :get, Object)
-        member(endpoint, :put, Object)
-        member(endpoint, :delete, Object)
-      end
-    end
-
-    dispatch.routes.find("PUT /accounts/3")
+    EarhartTestRouter.lookup(request: "verb=\"PUT\" resource=\"/accounts/3\"")
   end
 
   # I'm fairly sure this isn't how it's supposed to work.
   analysis.report("lotus-router lookup") do
-    dispatch = Lotus::Router.new do
-      resource "accounts"
-    end
+    LotusRouterRack.put("/accounts/3")
+  end
 
-    mount = Rack::MockRequest.new(dispatch)
-
-    mount.put("/accounts/3")
   end
 
   # I was unable to get this to work.

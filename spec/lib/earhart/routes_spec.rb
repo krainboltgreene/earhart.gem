e292e25e75d28c8c3a9135c33ff3d48b996b6317
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-08-31 02:00:36 -0500

    Adding the skeleton of tests for routes

--- spec/lib/earhart/routes_spec.rb
+++ spec/lib/earhart/routes_spec.rb
@@ -2,7 +2,30 @@ require "spec_helper"
 
 RSpec.describe Earhart::Routes do
   let(:routes) do
-    described_class.new
+    described_class.new(routes)
   end
 
+  describe "#add" do
+    let(:add) do
+      routes.add(pattern, receiver)
+    end
+
+
+  end
+
+  describe "#find" do
+    let(:find) do
+      routes.find(query)
+    end
+
+
+  end
+
+  describe "#to_s" do
+    let(:to_s) do
+      routes.to_s
+    end
+
+
+  end
 end

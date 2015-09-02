7b78a662201b7e501d54df12059810b9dfb4c09b
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-09-02 01:55:06 -0500

    Making it easier to detect performance issues

--- benchmarks/router_mapping.rb
+++ benchmarks/router_mapping.rb
@@ -14,10 +14,26 @@ Benchmark::IPS.options[:format] = :raw
 Benchmark.ips do |analysis|
   analysis.time = 5
   analysis.warmup = 2
+  analysis.compare!
 
-  analysis.report("Router Mapping") do
+  analysis.report("One map") do
     Earhart::Router.new do |router|
       router.collection(verb: :get, resource: :accounts, receiver: Object)
+    end
+  end
+
+  analysis.report("Two maps") do
+    Earhart::Router.new do |router|
+      router.collection(verb: :get, resource: :accounts, receiver: Object)
+      router.collection(verb: :put, resource: :accounts, receiver: Object)
+    end
+  end
+
+  analysis.report("Three maps") do
+    Earhart::Router.new do |router|
+      router.collection(verb: :get, resource: :accounts, receiver: Object)
+      router.collection(verb: :put, resource: :accounts, receiver: Object)
+      router.collection(verb: :pull, resource: :accounts, receiver: Object)
     end
   end
 end
